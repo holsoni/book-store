042bcb23de4e9e6c12763308b7414a74d5331ab3
holsoni <soniakostashchuk@gmail.com>
2023-08-31 21:00:03 +0300

    added BookRepository and BookService

--- src/main/java/com/example/bookstore/BookStoreApplication.java
+++ src/main/java/com/example/bookstore/BookStoreApplication.java
@@ -1,13 +1,39 @@
 package com.example.bookstore;
 
+import com.example.bookstore.model.Book;
+import com.example.bookstore.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BookStoreApplication {
+
+    private final BookService bookService;
+
+    @Autowired
+    public BookStoreApplication(BookService bookService) {
+        this.bookService = bookService;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(BookStoreApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return new CommandLineRunner() {
+            @Override
+            public void run(String... args) throws Exception {
+                Book bestBook = new Book();
+                bestBook.setTitle("Sapiens");
+                bestBook.setAuthor("Yuval Harari");
+                bestBook.setIsbn("9780062316097");
+                bookService.save(bestBook);
+                bookService.findAll().forEach(System.out::println);
+            }
+        };
+    }
 }
