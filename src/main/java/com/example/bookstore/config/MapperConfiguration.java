e5b4240f6dcfb00083077c265fd3952910fc6bf6
holsoni <soniakostashchuk@gmail.com>
2023-09-05 08:16:39 +0300

    changed usage for @MapperConfig

--- src/main/java/com/example/bookstore/config/MapperConfiguration.java
+++ src/main/java/com/example/bookstore/config/MapperConfiguration.java
@@ -1,13 +1,15 @@
 package com.example.bookstore.config;
 
 import org.mapstruct.InjectionStrategy;
+import org.mapstruct.MapperConfig;
 import org.mapstruct.NullValueCheckStrategy;
 
-@org.mapstruct.MapperConfig(
+@MapperConfig(
         componentModel = "spring",
         injectionStrategy = InjectionStrategy.CONSTRUCTOR,
         nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS,
         implementationPackage = "<PACKAGE_NAME>.impl"
 )
-public class MapperConfig {
+public class MapperConfiguration {
+
 }
--- src/main/java/com/example/bookstore/mapper/BookMapper.java
+++ src/main/java/com/example/bookstore/mapper/BookMapper.java
@@ -1,12 +1,12 @@
 package com.example.bookstore.mapper;
 
-import com.example.bookstore.config.MapperConfig;
+import com.example.bookstore.config.MapperConfiguration;
 import com.example.bookstore.dto.BookDto;
 import com.example.bookstore.dto.CreateBookRequestDto;
 import com.example.bookstore.model.Book;
 import org.mapstruct.Mapper;
 
-@Mapper(config = MapperConfig.class)
+@Mapper(config = MapperConfiguration.class)
 public interface BookMapper {
     BookDto toDto(Book book);
 
