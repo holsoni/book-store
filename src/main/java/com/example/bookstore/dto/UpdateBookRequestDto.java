a8d983ce385fb7fc195ac56f9567a640ad84bac8
holsoni <soniakostashchuk@gmail.com>
2023-09-07 11:31:42 +0300

    Added validation for DTO objects and GlobalExceptionHandler

--- src/main/java/com/example/bookstore/dto/UpdateBookRequestDto.java
+++ src/main/java/com/example/bookstore/dto/UpdateBookRequestDto.java
@@ -1,13 +1,24 @@
 package com.example.bookstore.dto;
 
+import com.example.bookstore.validation.Author;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import lombok.Data;
+import org.hibernate.validator.constraints.ISBN;
+import org.hibernate.validator.constraints.Length;
 
 @Data
 public class UpdateBookRequestDto {
+    @NotNull
+    @Length(min = 5, max = 200)
     private String title;
+    @Author
     private String author;
+    @ISBN
     private String isbn;
+    @NotNull
+    @Min(0)
     private BigDecimal price;
     private String description;
     private String coverImage;
---
+++ src/main/java/com/example/bookstore/exception/CustomGlobalExceptionHandler.java
@@ -0,0 +1,48 @@
+package com.example.bookstore.exception;
+
+import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class CustomGlobalExceptionHandler extends ResponseEntityExceptionHandler {
+    private static final String ERRORS = "errors";
+    private static final String TIMESTAMP = "timestamp";
+    private static final String STATUS = "status";
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
+            HttpHeaders headers,
+            HttpStatusCode status,
+            WebRequest request) {
+        Map<String, Object> responseBody = new LinkedHashMap<>();
+        List<String> errors = ex.getBindingResult().getAllErrors().stream()
+                        .map(this::getErrorMessage)
+                                .toList();
+        responseBody.put(ERRORS, errors);
+        responseBody.put(TIMESTAMP, LocalDateTime.now());
+        responseBody.put(STATUS, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(responseBody, headers, status);
+    }
+
+    private String getErrorMessage(ObjectError e) {
+        if (e instanceof FieldError) {
+            String field = ((FieldError) e).getField();
+            String message = e.getDefaultMessage();
+            return field + " " + message;
+        }
+        return e.getDefaultMessage();
+    }
+}
