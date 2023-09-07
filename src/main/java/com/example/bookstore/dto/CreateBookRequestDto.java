0879cc2ed26f0343e50aa6d7277aa5305a309af4
holsoni <soniakostashchuk@gmail.com>
2023-09-07 11:26:49 +0300

    deleted files

--- src/main/java/com/example/bookstore/dto/CreateBookRequestDto.java
+++ src/main/java/com/example/bookstore/dto/CreateBookRequestDto.java
@@ -1,23 +1,13 @@
 package com.example.bookstore.dto;
 
-import com.example.bookstore.validation.Author;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import lombok.Data;
-import org.hibernate.validator.constraints.ISBN;
 
 @Data
 public class CreateBookRequestDto {
-    @NotNull
     private String title;
-    @NotNull
-    @Author
     private String author;
-    @ISBN
     private String isbn;
-    @NotNull
-    @Min(0)
     private BigDecimal price;
     private String description;
     private String coverImage;
