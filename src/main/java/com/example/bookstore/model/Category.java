45066cfbf2731a74ddd45e3151b733d2d0c2e0c5
holsoni <soniakostashchuk@gmail.com>
2023-09-14 10:28:27 +0300

    added soft delete approach for Category

--- src/main/java/com/example/bookstore/model/Category.java
+++ src/main/java/com/example/bookstore/model/Category.java
@@ -7,9 +7,13 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 @Entity
 @Data
+@SQLDelete(sql = "UPDATE category SET is_deleted = true WHERE id=?")
+@Where(clause = "is_deleted=false")
 @Table(name = "category")
 public class Category {
     @Id
@@ -18,4 +22,5 @@ public class Category {
     @Column(nullable = false, unique = true)
     private String name;
     private String description;
+    private boolean isDeleted = false;
 }
