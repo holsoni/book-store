5b7550dc83a4fc39eb9b72d4f01bd4cd0763a349
holsoni <soniakostashchuk@gmail.com>
2023-09-16 19:08:39 +0300

    added CartItemDto & ShoppingCartDto

---
+++ src/main/java/com/example/bookstore/dto/shoppingcart/ShoppingCartDto.java
@@ -0,0 +1,11 @@
+package com.example.bookstore.dto.shoppingcart;
+
+import java.util.Set;
+import lombok.Data;
+
+@Data
+public class ShoppingCartDto {
+    private Long id;
+    private Long userId;
+    private Set<CartItemDto> cartItemsId;
+}
