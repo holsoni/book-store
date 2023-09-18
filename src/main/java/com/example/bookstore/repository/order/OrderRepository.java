229de2634fc70eb0a46519605262076cd85de586
holsoni <soniakostashchuk@gmail.com>
2023-09-18 14:55:03 +0300

    added repositories

---
+++ src/main/java/com/example/bookstore/repository/order/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.example.bookstore.repository.order;
+
+import com.example.bookstore.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
