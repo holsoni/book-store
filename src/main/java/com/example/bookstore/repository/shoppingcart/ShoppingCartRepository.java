d8ca6e00a4919e2c38c7516790ba11f978a9b541
holsoni <soniakostashchuk@gmail.com>
2023-09-19 23:45:33 +0300

    changed author name in liquibase, improved CartItem mapping and extracted method

--- src/main/java/com/example/bookstore/repository/shoppingcart/ShoppingCartRepository.java
+++ src/main/java/com/example/bookstore/repository/shoppingcart/ShoppingCartRepository.java
@@ -3,7 +3,12 @@ package com.example.bookstore.repository.shoppingcart;
 import com.example.bookstore.model.ShoppingCart;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ShoppingCartRepository extends JpaRepository<ShoppingCart, Long> {
+    @Query("FROM ShoppingCart sc "
+            + "LEFT JOIN FETCH sc.cartItems ci "
+            + "LEFT JOIN FETCH ci.book "
+            + "LEFT JOIN FETCH sc.user u WHERE u.id = :id ")
     Optional<ShoppingCart> findByUserId(Long id);
 }
