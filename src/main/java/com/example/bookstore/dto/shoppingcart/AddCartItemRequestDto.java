a3c490dbc7aa0f705c19fb27e3ea00a25853e06f
holsoni <soniakostashchuk@gmail.com>
2023-09-19 23:45:33 +0300

    changed author name in liquibase, improved CartItem mapping and extracted method

--- src/main/java/com/example/bookstore/dto/shoppingcart/AddCartItemRequestDto.java
+++ src/main/java/com/example/bookstore/dto/shoppingcart/AddCartItemRequestDto.java
@@ -1,9 +1,11 @@
 package com.example.bookstore.dto.shoppingcart;
 
+import jakarta.validation.constraints.Min;
 import lombok.Data;
 
 @Data
 public class AddCartItemRequestDto {
     private Long bookId;
+    @Min(1)
     private int quantity;
 }
