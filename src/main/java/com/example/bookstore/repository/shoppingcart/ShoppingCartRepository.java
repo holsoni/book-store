cce78c4862933e6f2bc60a7dd57a2d15333c94a9
holsoni <soniakostashchuk@gmail.com>
2023-09-25 17:51:37 +0300

    some changes for resolve conflict

--- src/main/java/com/example/bookstore/repository/shoppingcart/ShoppingCartRepository.java
+++ src/main/java/com/example/bookstore/repository/shoppingcart/ShoppingCartRepository.java
@@ -3,7 +3,6 @@ package com.example.bookstore.repository.shoppingcart;
 import com.example.bookstore.model.ShoppingCart;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface ShoppingCartRepository extends JpaRepository<ShoppingCart, Long> {
     Optional<ShoppingCart> findByUserId(Long id);
