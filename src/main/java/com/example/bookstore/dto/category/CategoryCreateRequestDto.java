1c2e6d5601384755597bd228637ef0be6d507975
holsoni <soniakostashchuk@gmail.com>
2023-09-25 17:44:30 +0300

    added tests for findAll(), findById(), save(), deleteById()

--- src/main/java/com/example/bookstore/dto/category/CategoryCreateRequestDto.java
+++ src/main/java/com/example/bookstore/dto/category/CategoryCreateRequestDto.java
@@ -2,9 +2,11 @@ package com.example.bookstore.dto.category;
 
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
+import lombok.experimental.Accessors;
 import org.hibernate.validator.constraints.Length;
 
 @Data
+@Accessors(chain = true)
 public class CategoryCreateRequestDto {
     @NotEmpty
     @Length(min = 3, max = 50)
