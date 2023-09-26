fa36ffea36bc648ce71b68b4f8c4ba0f801ea6af
holsoni <soniakostashchuk@gmail.com>
2023-09-26 09:55:06 +0300

    written tests for BookController

--- src/main/java/com/example/bookstore/repository/user/UserRepository.java
+++ src/main/java/com/example/bookstore/repository/user/UserRepository.java
@@ -3,6 +3,7 @@ package com.example.bookstore.repository.user;
 import com.example.bookstore.model.User;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
